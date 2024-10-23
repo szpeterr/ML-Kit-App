9e2fa7c6693bcc3fb7a512010e322a6dc57558cd
Ysaault <toth.zsuzsanna831@gmail.com>
2024-10-23 22:57:14 +0200

    kerültbele még egy activity, meg a gobja is működik

--- app/src/main/java/com/example/mlkitapp/MainActivity.java
+++ app/src/main/java/com/example/mlkitapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mlkitapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
 
 
         Configuration = findViewById(R.id.config);
+        Configuration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Configuration.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
 
     }
 }
