b356e4a26836b9e58ab6af6eeb22635282650bb9
Ysaault <toth.zsuzsanna831@gmail.com>
2024-10-23 22:48:08 +0200

    kerültbele még egy activity

--- app/src/main/java/com/example/mlkitapp/Configuration.java
+++ app/src/main/java/com/example/mlkitapp/Configuration.java
@@ -1,38 +1,24 @@
 package com.example.mlkitapp;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    private CardView Camera;
-    private CardView Audio;
-    private CardView Face;
-    private CardView Configuration;
+public class Configuration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_configuration);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Camera = findViewById(R.id.camera);
-
-
-        Configuration = findViewById(R.id.config);
-
     }
 }
