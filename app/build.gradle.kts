29c6bdac54cd65b4c1038acfe6b14b4ac2320af2
szpeterr <peter.szerencsi.bp@gmail.com>
2024-10-24 01:57:43 +0200

    Before fixing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,12 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.face.detection)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.camera.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
