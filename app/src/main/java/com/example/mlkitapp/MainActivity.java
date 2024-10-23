9c6db07205bef1790963f30fed040dc77814278e
szpeterr <peter.szerencsi.bp@gmail.com>
2024-10-23 23:33:50 +0200

    Pose and Face buttons work

--- app/src/main/java/com/example/mlkitapp/MainActivity.java
+++ app/src/main/java/com/example/mlkitapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mlkitapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.card.MaterialCardView;
 
+public class MainActivity extends AppCompatActivity {
+    MaterialCardView cardFaceDetection;
+    MaterialCardView cardPoseDetection;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,23 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        cardFaceDetection = findViewById(R.id.cardFaceDetection);
+        cardPoseDetection = findViewById(R.id.cardPoseDetection);
+
+        cardFaceDetection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, FaceDetectionActivity.class);
+                startActivity(myIntent);
+            }
+        });
+        cardPoseDetection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, PoseDetectionActivity.class);
+                startActivity(myIntent);
+            }
         });
     }
 }
