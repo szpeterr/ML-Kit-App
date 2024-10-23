21b7cce2891e0ab10508c3c47c5278ef7c6baa83
szpeterr <peter.szerencsi.bp@gmail.com>
2024-10-23 23:35:20 +0200

    Small fixes

--- app/src/main/java/com/example/mlkitapp/MainActivity.java
+++ app/src/main/java/com/example/mlkitapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.mlkitapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,19 +27,13 @@ public class MainActivity extends AppCompatActivity {
         cardFaceDetection = findViewById(R.id.cardFaceDetection);
         cardPoseDetection = findViewById(R.id.cardPoseDetection);
 
-        cardFaceDetection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, FaceDetectionActivity.class);
-                startActivity(myIntent);
-            }
+        cardFaceDetection.setOnClickListener(v -> {
+            Intent myIntent = new Intent(MainActivity.this, FaceDetectionActivity.class);
+            startActivity(myIntent);
         });
-        cardPoseDetection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, PoseDetectionActivity.class);
-                startActivity(myIntent);
-            }
+        cardPoseDetection.setOnClickListener(v -> {
+            Intent myIntent = new Intent(MainActivity.this, PoseDetectionActivity.class);
+            startActivity(myIntent);
         });
     }
 }
