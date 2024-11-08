420a45c7943d2e9864b6d23588744e556f25005e
szpeterr <151888386+szpeterr@users.noreply.github.com>
2024-11-08 21:14:19 +0100

    Small optimizations

--- app/src/main/java/com/example/ml_vision_app/MainActivity.java
+++ app/src/main/java/com/example/ml_vision_app/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.ml_vision_app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
